8c78f489c514a4b7435ac52fa84ab45b73a183f7
Tharindra <tharindragayashika@gmail.com>
2024-06-18 19:54:37 +0530

    application.properties is updated

--- src/main/java/com/testify/Testify_Backend/enums/UserRole.java
+++ src/main/java/com/testify/Testify_Backend/enums/UserRole.java
@@ -1,6 +1,6 @@
 package com.testify.Testify_Backend.enums;
 
-public enum Role {
+public enum UserRole {
     USER,
     ADMIN,
     ORG_ADMIN
--- src/main/java/com/testify/Testify_Backend/model/User.java
+++ src/main/java/com/testify/Testify_Backend/model/User.java
@@ -1,18 +1,34 @@
 package com.testify.Testify_Backend.model;
 
-import com.testify.Testify_Backend.enums.Role;
+import com.testify.Testify_Backend.enums.UserRole;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @Entity
 @Getter
 @Setter
+@EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class User {
+public class User implements UserDetails {
+
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
     private long id;
 
     @Column(name = "user_name", unique=true)
@@ -27,9 +43,58 @@ public class User {
     @Column(unique = true, nullable = false)
     private String email;
 
+
     @Column(nullable = false)
     private String password;
 
     @Enumerated(EnumType.STRING)
-    private Role role;
+    private UserRole userRole;
+
+
+    private Boolean locked;
+
+    private Boolean enabled;
+
+
+    public User(String userName, String firstName, String lastName, String email, String password, UserRole userRole, Boolean locked, Boolean enabled) {
+        this.userName = userName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.userRole = userRole;
+        this.locked = locked;
+        this.enabled = enabled;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(userRole.name());
+        return Collections.singletonList(authority);
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !locked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
 }
