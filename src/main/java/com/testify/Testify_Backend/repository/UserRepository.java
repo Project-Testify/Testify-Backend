428782cdc80b1b36e5fa1f7e80498c69588630b2
raviendalpatadu <102800797+raviendalpatadu@users.noreply.github.com>
2024-09-18 15:14:38 +0530

    reject req done 2

--- src/main/java/com/testify/Testify_Backend/repository/UserRepository.java
+++ src/main/java/com/testify/Testify_Backend/repository/UserRepository.java
@@ -1,14 +1,12 @@
 package com.testify.Testify_Backend.repository;
 
 import com.testify.Testify_Backend.model.User;
-import com.testify.Testify_Backend.responses.GenericResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
@@ -23,7 +21,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
             "SET u.enabled = TRUE WHERE u.email = ?1")
     int enableUser(String email);
 
-//  verfiy Organization
+//  verify Organization
     @Transactional
     @Modifying
     @Query("UPDATE User u SET u.verified = TRUE where u.id = ?1")
