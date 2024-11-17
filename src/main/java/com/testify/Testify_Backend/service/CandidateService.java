23f8052efe28fabd7d39496dac03d108b6b69cfc
VIHANGAGIT <vihangahansanakv@gmail.com>
2024-11-17 19:36:27 +0530

    Organization view API for candidate

--- src/main/java/com/testify/Testify_Backend/service/CandidateService.java
+++ src/main/java/com/testify/Testify_Backend/service/CandidateService.java
@@ -1,13 +1,16 @@
 package com.testify.Testify_Backend.service;
 
 import com.testify.Testify_Backend.model.Candidate;
+import com.testify.Testify_Backend.model.Organization;
 import com.testify.Testify_Backend.responses.candidate_management.CandidateExam;
 import com.testify.Testify_Backend.responses.candidate_management.CandidateProfile;
+import com.testify.Testify_Backend.responses.candidate_management.OrganizationCandidateView;
 
 import java.util.List;
 
 public interface CandidateService {
     public List<CandidateExam> getCandidateExams(String status);
+    public List<OrganizationCandidateView> getOrganizations();
     public CandidateProfile getCandidateProfile();
     public String updateCandidateProfile(Candidate candidate);
     public String deleteCandidateProfile(long id);
